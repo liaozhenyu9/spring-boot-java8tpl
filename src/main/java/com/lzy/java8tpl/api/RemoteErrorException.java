faf727ba1bacb34530cb48079288873f1291a7db
liaozhenyu <liaozhenyu@ubox.cn>
2024-03-13 10:49:35 +0800

    RemoteErrorException

--- src/main/java/com/lzy/java8tpl/api/RemoteErrorException.java
+++ src/main/java/com/lzy/java8tpl/api/RemoteErrorException.java
@@ -2,6 +2,8 @@ package com.lzy.java8tpl.api;
 
 public class RemoteErrorException extends ApiException {
 
+    private String extSystemErrorCode;
+
     public RemoteErrorException() {
         super(ErrorCode.REMOTE_ERROR);
     }
@@ -16,6 +18,20 @@ public class RemoteErrorException extends ApiException {
 
     public RemoteErrorException(String msg, Throwable cause) {
         super(ErrorCode.REMOTE_ERROR, msg, cause);
+    }
+
+    public RemoteErrorException(String extSystemErrorCode, String msg) {
+        super(ErrorCode.REMOTE_ERROR, msg);
+        this.extSystemErrorCode = extSystemErrorCode;
+    }
+
+    public RemoteErrorException(String extSystemErrorCode, String msg, Throwable cause) {
+        super(ErrorCode.REMOTE_ERROR, msg, cause);
+        this.extSystemErrorCode = extSystemErrorCode;
+    }
+
+    public String getExtSystemErrorCode() {
+        return extSystemErrorCode;
     }
 
 }
