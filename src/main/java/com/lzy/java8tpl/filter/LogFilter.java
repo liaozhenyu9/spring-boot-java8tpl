fad4c715979a89bfcfb7832d986b1a5b1d1e4cc6
liaozhenyu <liaozhenyu@ubox.cn>
2024-03-15 11:41:49 +0800

    //忽略文件上传，防止影响MultipartFile接参

--- src/main/java/com/lzy/java8tpl/filter/LogFilter.java
+++ src/main/java/com/lzy/java8tpl/filter/LogFilter.java
@@ -51,11 +51,10 @@ public class LogFilter extends OncePerRequestFilter {
             stopWatch.start();
             log.info("<<< REQUEST [{}] [{}] [{}] [{}]", request.getRequestURI(), request.getMethod(), request.getProtocol(), request.getRemoteAddr());
             log.info("<<< REQUEST HEADERS   : {}", JSON.toJSONString(HttpUtils.getRequestHeader(request)));
-            ////会影响文件上传MultipartFile接参
             if (!CollectionUtils.isEmpty(request.getParameterMap())) {
                 log.info("<<< REQUEST PARAMETERS: {}", JSON.toJSONString(request.getParameterMap()));
             }
-            //忽略文件上传
+            //忽略文件上传，防止影响MultipartFile接参
             if (HttpUtils.isFileUploadContentType(request)) {
                 chain.doFilter(request, response);
                 return;
